926657476e017a32f09efe05e0c375df7d22a561
hyeon <lhyeonnit@gmail.com>
2024-02-05 17:33:35 +0900

    login

--- src/main/java/shop/mtcoding/blog/user/UserController.java
+++ src/main/java/shop/mtcoding/blog/user/UserController.java
@@ -1,11 +1,20 @@
-package shop.mtcoding.blog.controller;
+package shop.mtcoding.blog.user;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
-
+    @PostMapping("/login")
+    public String login(){
+        return "redirect:/";
+    }
+    @PostMapping("/join")
+    public String join(UserRequest.JoinDTO requsetDTO){
+        System.out.println(requsetDTO);
+        return "user/loginForm";
+    }
     @GetMapping("/joinForm")
     public String joinForm() {
         return "user/joinForm";
---
+++ src/main/java/shop/mtcoding/blog/user/UserRepository.java
@@ -0,0 +1,11 @@
+package shop.mtcoding.blog.user;
+
+//@Repository
+public class UserRepository {
+//    private EntityManager em;
+//
+//    public UserRepository(EntityManager em) {
+//        this.em = em;
+//    }
+
+}
