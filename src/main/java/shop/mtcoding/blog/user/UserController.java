fb832b0fa77af9fb28f11411a911386d7022c04c
hyeon <lhyeonnit@gmail.com>
2024-02-14 16:49:55 +0900

    UserController

--- src/main/java/shop/mtcoding/blog/user/UserController.java
+++ src/main/java/shop/mtcoding/blog/user/UserController.java
@@ -1,16 +1,23 @@
 package shop.mtcoding.blog.user;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import shop.mtcoding.blog._core.config.security.MyLoginUser;
+
 @RequiredArgsConstructor
 @Controller
 public class UserController {
     private final UserRepository userRepository;
     private final HttpSession session;
-//    @PostMapping("/login")
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    //    @PostMapping("/login")
 //    public String login(UserRequest.LoginDTO requestDTO){
 //        System.out.println(requestDTO);
 //        if (requestDTO.getUsername().length() < 3) {
@@ -27,8 +34,11 @@ public class UserController {
     @PostMapping("/join")
     public String join(UserRequest.JoinDTO requsetDTO){
         System.out.println(requsetDTO);
+        String rawPassword = requsetDTO.getPassword();
+        String encPassword = passwordEncoder.encode(rawPassword);
+        requsetDTO.setPassword(encPassword);
         userRepository.save(requsetDTO);
-        return "user/loginForm";
+        return "redirect:/loginForm";
     }
     @GetMapping("/joinForm")
     public String joinForm() {
@@ -41,7 +51,9 @@ public class UserController {
     }
 
     @GetMapping("/user/updateForm")
-    public String updateForm() {
+    public String updateForm(HttpServletRequest request, @AuthenticationPrincipal MyLoginUser myLoginUser) {
+        User user = userRepository.findByUsername(myLoginUser.getUsername());
+        request.setAttribute("user",user);
         return "user/updateForm";
     }
 
